52dfb5194f730459dd98fdd18cc43fdbccedf5a0
Asger523 <asger.barming@gmail.com>
2025-04-07 10:23:22 +0200

    Added stack navigation (may need to be customized later)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
-import GoogleButton from './app/components/GoogleButton';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SignIn from './app/screens/signIn';
 
 GoogleSignin.configure({
   webClientId:
@@ -9,12 +11,29 @@ GoogleSignin.configure({
   iosClientId:
     '180962452617-de5ofae0npqag3ufmp4lhnjd56759lo8.apps.googleusercontent.com',
 });
+//Initialize the stack navigator
+const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // Default options for all screens under this navigator.
+  const scrOptions = {
+    headerStyle: {backgroundColor: '#6200ee'},
+    headerTitleStyle: {color: 'white'},
+    headerBackTitleVisible: false,
+    headerTintColor: 'white',
+  };
+
   return (
-    <SafeAreaView style={styles.background}>
-      <GoogleButton />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={scrOptions}>
+        <Stack.Screen
+          name="SignIn"
+          options={{headerShown: false}}
+          component={SignIn}
+        />
+        {/*Add screens here*/}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
